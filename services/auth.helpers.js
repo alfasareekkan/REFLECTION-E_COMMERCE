25b123a8844c1fda752d027a6fc1240a74477f71
MOHAMMED ALFAS A <87485303+alfasareekkan@users.noreply.github.com>
2022-10-08 05:47:06 +0530

    add userside profile button

--- services/auth.helpers.js
+++ services/auth.helpers.js
@@ -9,7 +9,7 @@ module.exports = {
             if (user) {
                 responce.status = true
                 resolve(responce)
-            }
+             }
             else {
                 resolve(responce)
              }
